89ee366115f378a1c6d7a5f3de4dfa321cd49aaa
k-pulkit <pulkit42041@gmail.com>
2022-10-21 13:43:28 -0400

    New Changes

--- src/main/java/com/pknn/javacourse/lesson13/CarService.java
+++ src/main/java/com/pknn/javacourse/lesson13/CarService.java
@@ -7,6 +7,7 @@ public class CarService {
     private final Logger logger = LoggerFactory.getLogger(CarService.class);
 
     public void process(String s) {
+        // Only if debugging is enabled will the s be concat
         logger.debug("This is debug message - {}", s);
     }
 
